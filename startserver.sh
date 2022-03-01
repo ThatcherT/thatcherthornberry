a1a15f6ecb13e1768ed2be878d8b9667be4aa52b
thatcher thornberry <thatcherthornberry@gmail.com>
2022-02-28 20:47:02 -0600

    full path

--- startserver.sh
+++ startserver.sh
@@ -1,4 +1,4 @@
 # remove danlging images
 sudo docker system prune -f
 # start docker containers
-sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "/home/sa_109683913549673363733/thatcherthornberry:/home/sa_109683913549673363733/thatcherthornberry" -w="/home/sa_109683913549673363733/thatcherthornberry" docker/compose:1.29.2 -f projects/spotifly/docker-compose.yaml -f projects/me/docker-compose.yaml -f docker-compose.yml up --force-recreate --remove-orphans -d --build
+sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "/home/sa_109683913549673363733/thatcherthornberry:/home/sa_109683913549673363733/thatcherthornberry" -w="/home/sa_109683913549673363733/thatcherthornberry" docker/compose:1.29.2 -f /home/sa_109683913549673363733/thatcherthornberry/projects/spotifly/docker-compose.yaml -f /home/projects/me/docker-compose.yaml -f docker-compose.yml up --force-recreate --remove-orphans -d --build
