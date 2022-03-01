b3fc17e2df32857cd62b8973a968972f240308cf
thatcher thornberry <thatcherthornberry@gmail.com>
2022-02-28 20:11:03 -0600

    add compose file to server script

--- startserver.sh
+++ startserver.sh
@@ -1,4 +1,4 @@
 # remove danlging images
 sudo docker system prune -f
 # start docker containers
-sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "/home/sa_109683913549673363733/spotifly:/home/sa_109683913549673363733/spotifly" -w="/home/sa_109683913549673363733/spotifly" docker/compose:1.29.2 -f projects/spotifly/docker-compose.yaml -f projects/me/docker-compose.yaml up --force-recreate --remove-orphans -d --build
+sudo docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "/home/sa_109683913549673363733/spotifly:/home/sa_109683913549673363733/spotifly" -w="/home/sa_109683913549673363733/spotifly" docker/compose:1.29.2 -f projects/spotifly/docker-compose.yaml -f projects/me/docker-compose.yaml -f docker-compose.yml up --force-recreate --remove-orphans -d --build
