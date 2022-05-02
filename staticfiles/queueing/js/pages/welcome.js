b43313437d301dc11a20a13e88da8b4e5d706446
Thatcher Thornberry <thatcherthornberry@gmail.com>
2022-05-02 02:18:29 +0000

    Squashed 'projects/spotifly/' changes from 5228f68..fb9d4af
    fb9d4af static files oops!
    
    git-subtree-dir: projects/spotifly
    git-subtree-split: fb9d4af0f587aaf728baa5fd855764f3260adb21

--- staticfiles/queueing/js/pages/welcome.js
+++ staticfiles/queueing/js/pages/welcome.js
@@ -14,27 +14,19 @@ function loadWelcomePage() {
                 </p>
             </div>
         </div>`;
-    // when click get-started button, fade out main text
+    // when click get-started button, show the dj form
     document.getElementById("get-started").addEventListener("click", function () {
-        mainContent.classList.add("fade-out");
         mainContent.classList.add("dj-form");
-        const getStarted = document.getElementById("get-started");
-        getStarted.classList.add("fade-out");
-
-        // wait for fade out to complete
-        setTimeout(function () {
-            mainContent.innerHTML = djFormHeader;
-            getStarted.style.display = "none";
-            mainContent.classList.remove("main-text");
-            mainContent.classList.add("form-head");
-            mainContent.classList.remove("fade-out");
-            mainContent.classList.add("fade-in");
-        }, 1000);
+        mainContent.innerHTML = djFormHeader;
+        document.getElementById("get-started").style.display = "none";
+        mainContent.classList.remove("main-text");
+        mainContent.classList.add("form-head");
     });
 
     document.getElementById("get-started").style.display = "";
 }
 
+// the form to choose between following a dj or becoming one
 var djFormHeader = `
     <div class="row">
         <div class="col-12">
@@ -57,25 +49,34 @@ var djFormHeader = `
     </div>`;
 
 
+// shows the form to follow a dj
 function followDJButton() {
     mainContent.innerHTML =
         djFormHeader +
-        `
+        `<div class="row">
+                <div class="col-12">
+                <select id="dj-select" class="selectpicker" data-style="btn-lg big-ole-btn" data-size="10" data-live-search="true">
+                        <option selected disabled>Select a DJ</option>
+                        ${djObj.djs.map(dj => `<option value="${dj}">${dj}</option>`).join("")}
+                    </select>
+                </div>
+            </div>
             <div class="row">
                 <div class="col-12">
-                    <form id="follow-dj-form" onSubmit="return followDJ();">
-                        <div class="form-group">
-                            <label for="follow-dj">DJ Name</label>
-                            <input type="text" class="form-control big-ole-form-input" id="follow-dj" required>
-                        </div>
-                        <button id="follow-dj-btn" class="btn btn-primary btn-lg form-submit">
-                            Submit
-                        </button>
-                    </form>
+                    <button id="follow-dj-btn" class="btn btn-primary btn-lg form-submit big-ole-btn" onClick="followDJ()">
+                        Submit
+                    </button>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p id="follow-dj-error" class="error-message"></p>
                 </div>
             </div>`;
+    $('#dj-select').selectpicker().selectpicker('refresh');
 }
 
+// shows the form to become a dj
 function becomeDJButton() {
     $.ajax({
         url: "/spotify/connect-link/",
