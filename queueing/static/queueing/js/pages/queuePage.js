6c8d1dab353338b6809c49518addb0c953b3c758
Thatcher Thornberry <thatcherthornberry@gmail.com>
2022-05-02 04:35:50 +0000

    Squashed 'projects/spotifly/' changes from 633f2b6..705efc5
    705efc5 guard rails.. for the general public of course
    
    git-subtree-dir: projects/spotifly
    git-subtree-split: 705efc5d2945dcb554c507543bc810a1bbc88f4c

--- queueing/static/queueing/js/pages/queuePage.js
+++ queueing/static/queueing/js/pages/queuePage.js
@@ -2,25 +2,33 @@
 // Queue Page
 function loadQueuePage() {
     updateActiveIcon(document.getElementById("queue-icon"));
-    mainContent.innerHTML = "";
-    mainContent.innerHTML = `
-      <p id="queue-message"></p>
-      <div class="row">
-          <div class="col-12">
-              <p>
-                  Queue Songs
-              </p>
-          </div>
-      </div>
-      <div class="row">
-          <div class="col-12">
-              <div id="queue-container" class="queue-container">
-                  <input type="text" class="form-control big-ole-form-input" id="queue-song-input" placeholder="Search for a song">
-              </div>
-              <button class="btn btn-primary big-ole-btn" onclick="search()">Search</button>
-          </div>
-      </div>
-      <div id="search-results" class="search-box">
-      </div>
-      `;
+
+    // see if user is following a dj
+
+    if (getFollowingDJ()) {
+
+        mainContent.innerHTML = "";
+        mainContent.innerHTML = `
+        <p id="queue-message"></p>
+        <div class="row">
+            <div class="col-12">
+                <p>
+                    Queue Songs
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div id="queue-container" class="queue-container">
+                    <input type="text" class="form-control big-ole-form-input" id="queue-song-input" placeholder="Search for a song">
+                </div>
+                <button class="btn btn-primary big-ole-btn" onclick="search()">Search</button>
+            </div>
+        </div>
+        <div id="search-results" class="search-box">
+        </div>
+        `;
+    } else {
+        mainContent.innerHTML = "You aren't following a DJ. Can't queue!";
+    }
 }
