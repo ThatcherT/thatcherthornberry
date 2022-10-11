d337edb64c4031f15fc6fd3d88f88e8d87848ce0
Thatcher Thornberry <thatcherthornberry@gmail.com>
2022-10-11 01:33:09 +0000

    Squashed 'projects/spotifly/' changes from b1158cf..4964f29
    4964f29 sensibledefaults
    
    git-subtree-dir: projects/spotifly
    git-subtree-split: 4964f291fef53d5ec585246b65d7445feb20ad3b

--- music/settings.py
+++ music/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "music.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-if config("LOCAL") and not config("LOCALPOSTGRES"):
+if config("LOCAL", default=False, cast=bool) and not config("LOCALPOSTGRES", default=False, cast=bool):
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
