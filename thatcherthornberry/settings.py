cabf6d8e8d6b7be8dea7e3a35e2b83ffcdb3eb01
Thatcher Thornberry <thatcherthornberry@gmail.com>
2022-10-16 18:44:53 +0000

    Squashed 'projects/me/' changes from 7b3da19..b85baf9
    b85baf9 missing static
    
    git-subtree-dir: projects/me
    git-subtree-split: b85baf944813c1b4591bec3ebc06f8b11e79e6f2

--- thatcherthornberry/settings.py
+++ thatcherthornberry/settings.py
@@ -137,9 +137,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.2/howto/static-files/
-
 STATIC_URL = "/staticfiles/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
